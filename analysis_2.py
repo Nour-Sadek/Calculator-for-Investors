39ba0059545a9db5dbf9b00603f847aab2c9c4f6
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-08-04 10:49:31 +0300

    Added the main_menu(), crud_menu(), and top_ten_menu() functions

--- analysis_2.py
+++ analysis_2.py
@@ -58,3 +58,109 @@ if c.fetchone() is None:
                 'INSERT INTO financial VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
                 values_of_row)
             conn.commit()
+
+def main_menu():
+    while True:
+        print("""
+MAIN MENU
+0 Exit
+1 CRUD operations
+2 Show top ten companies by criteria\n""")
+
+        print("Enter an option:")
+        user_input = input()
+        if user_input not in ['0', '1', '2']:
+            print("Invalid option!")
+        else:
+            if user_input == '0':
+                print('Have a nice day!')
+                break
+            elif user_input == '1':
+                crud_menu()
+            else:
+                top_ten_menu()
+
+
+def crud_menu():
+    print("""
+CRUD MENU
+0 Back
+1 Create a company
+2 Read a company
+3 Update a company
+4 Delete a company
+5 List all companies\n""")
+
+    print("Enter an option:")
+    user_input = input()
+    if user_input not in ['0', '1', '2', '3', '4', '5']:
+        print("Invalid option!")
+    else:
+        if user_input == '1':  # DONE!
+            create_company()
+        elif user_input == '2':  # DONE!
+            read_company()
+        elif user_input == '3':  # DONE!
+            update_company()
+        elif user_input == '4':  # DONE!
+            delete_company()
+        elif user_input == '5':  # DONE!
+            list_companies()
+        else:  # user asked to go back
+            pass
+
+
+def top_ten_menu():
+    print("""
+TOP TEN MENU
+0 Back
+1 List by ND/EBITDA
+2 List by ROE
+3 List by ROA\n""")
+
+    print("Enter an option:")
+    user_input = input()
+    if user_input not in ['0', '1', '2', '3']:
+        print("Invalid option!")
+    else:
+        if user_input == '1':
+            list_by_nd()
+        elif user_input == '2':
+            list_by_roe()
+        elif user_input == '3':
+            list_by_roa()
+        else:  # user asked to go back
+            pass
+
+# CRUD MENU actions
+def create_company():
+    print('Not implemented!\n')
+
+
+def read_company():
+    print('Not implemented!\n')
+
+
+def update_company():
+    print('Not implemented!\n')
+
+
+def delete_company():
+    print('Not implemented!\n')
+
+
+def list_companies():
+    print('Not implemented!\n')
+
+
+# TOP TEN MENU actions
+def list_by_nd():
+    print('Not implemented!\n')
+
+
+def list_by_roe():
+    print('Not implemented!\n')
+
+
+def list_by_roa():
+    print('Not implemented!\n')
